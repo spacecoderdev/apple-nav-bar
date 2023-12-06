e674e182b71b475c7e377fcc81e31154cd5c38c1
Milo <146677344+spacecoderdev@users.noreply.github.com>
2023-12-06 01:01:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const navButton = document.querySelector('.nav-button');
+const nav = document.querySelector('.nav');
+
+navButton.addEventListener('click', () => {
+    const visibility = navButton.getAttribute('aria-expanded');
+    
+    if (visibility === 'false') {
+        navButton.setAttribute('aria-expanded', true);
+        nav.setAttribute('data-visible', true);
+    } else {
+        navButton.setAttribute('aria-expanded', false);
+        nav.setAttribute('data-visible', false);
+    }
+});
